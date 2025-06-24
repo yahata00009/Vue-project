02c7ee605294c13673d2ba033460fc92e163c855
kohei.yahata <kohei.yahata.pu@ambl.co.jp>
2025-06-24 11:38:31 +0900

    update vuetify追加

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,5 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // Vuetifyのスタイルシートを追加
+  css: ['vuetify/lib/styles/main.sass'],
+  // Vuetifyをトランスパイル対象に追加
+  build: {
+    transpile: ['vuetify'],
+  },
+  // モジュール設定
+  modules: [
+    // '@nuxt/eslint',
+    // '@nuxt/image',
+    // VuetifyのViteプラグインを登録する設定
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins ||= []
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
 })
